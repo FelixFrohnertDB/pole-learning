1d75ff1435592542825ae314e7c7499c054a3b9a
felixfrohnertdb <f.frohnert@liacs.leidenuniv.nl>
2025-03-11 17:07:26 +0100

    update in code structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codebase",  # Your package name
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if needed
+)
